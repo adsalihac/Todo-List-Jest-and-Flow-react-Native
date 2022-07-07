6be92c5e8943fdcdea39102cdbdf08904bbe8df1
adsalihac <adsalihac@gmail.com>
2022-07-07 16:50:05 +0530

    test issues solved

--- src/app.js
+++ src/app.js
@@ -5,41 +5,70 @@ import React, { Component } from 'react'
 import Button from './button'
 import Input from './input'
 
-import type {Node} from 'react'
+import type { Node } from 'react'
+import { types } from '@babel/core'
 
-type PropsType = {||};
+// type ConnectPropsType = {
+//   addItem: () => Promise<void>,
+// }
+
+type PropsType = {|
+  |};
 
 type StateType = {
-    data : Array<Object>,
-    text : string,
+  data: Array<Object>,
+  text: string,
 };
 
-class App extends Component<PropsType, StateType> {
-    constructor(props: PropsType) {
-        super(props);
-        this.state = {
-          data: [{},{}],
-          text: '',
-        };
-    }
 
-  render() : Node {
+
+class App extends Component<PropsType, StateType> {
+  constructor(props: PropsType) {
+    super(props);
+    this.state = {
+      data: [{ id: 1, name: 'data1' }, { id: 2, name: 'data2' }, { id: 3, name: 'data3' }],
+      text: '',
+    };
+  }
+
+ 
+  setData: (field: string) =>  (value: string) => void = 
+  (field: string) => (value: string) => {
+    this.setState({[field]: value});
+  }
+
+  addItem: () => void = () => {
+      const { data, text }
+      = this.state;
+      const newData = { id: data.length + 1, name: text };
+      this.setState({ data: [...data, newData], text: '' });
+      console.log("newData" ,JSON.stringify( this.state.data));
+  }
+
+  render(): Node {
+
+    const {
+      addItem,
+      setData,
+      state: { data, text }
+    } = this;
+
+   
     return (
-      <View style={{flex:1 , backgroundColor:"white"}}>
-        <View style={{flexDirection:'row' , alignItems:'center'}}>
+      <View style={{ flex: 1, backgroundColor: "white" }}>
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
           <Input
             placeholder={"Enter task"}
-            value={this.state.text}
-            onChangeText={(text) => this.setState({text : text})}
+            value={text}
+            onChangeText={setData('text')}
           />
+    
           <Button
-          title="Click"
-          onPress={() => {
-            console.log("Button Clicked");
-          }}
-          tilteStyle={{ fontSize : 14}}
-          style={{backgroundColor:"blue", width:80, height:40 , borderRadius: 5, justifyContent:"center", alignItems:"center" , marginRight:5}}
-        />
+            title="Click"
+            onPress={addItem}
+            tilteStyle={{ fontSize: 14 }}
+            style={{ backgroundColor: "blue", width: 80, height: 40, borderRadius: 5, justifyContent: "center", alignItems: "center", marginRight: 5 }}
+          />
         </View>
       </View>
     )
--- src/app.spec.js
+++ src/app.spec.js
@@ -1,7 +1,11 @@
 // @flow
 
 import React from 'react';
+import { findAllWithType, findWithType } from 'react-shallow-testutils';
 import ShallowRenderer from 'react-test-renderer/shallow';
+
+import Input from './input'
+import Button from './button'
 
 import App from './app';
 
@@ -21,4 +25,85 @@ describe('app', () => {
         const result = renderApp();
         expect(result).toMatchSnapshot();
     });
+
+    it('renders a button', () => {
+        const result = renderApp();
+        const buttons = findAllWithType(result, Button);
+        expect(buttons.length).toBe(1);
+    }
+    );
+
+    it('renders an input', () => {
+        const result = renderApp();
+        const inputs = findAllWithType(result, Input);
+        expect(inputs.length).toBe(1);
+    }
+    );
+
+    it('renders button with correct action', () => {
+        const result = renderApp();
+
+        const taskInput = findWithType(result, Input);
+
+        const taskname = 'taskname';
+
+        taskInput.props.onChangeText(taskname);
+
+        const button = findWithType(result, Button);
+        button.props.onPress();
+    });
+
+    it('renders button with wrong action', () => {
+        const result = renderApp();
+        const instance = renderer.getMountedInstance();
+
+        const taskInput = findWithType(result, Input);
+
+
+        taskInput.props.onChangeText('');
+        expect(instance.state.text).toBe(''); //true
+
+        console.log("TEXT", instance.state.text);
+
+        const button = findWithType(result, Button);
+        button.props.onPress();
+    });
+
+
+    // it('renders a list', () => {
+    //     const result = renderApp();
+    //     const list = findWithType(result, 'ul');
+    //     expect(list).toBeDefined();
+    // }
+    // );
+
+    // it('renders a list item', () => {
+    //     const result = renderApp();
+    //     const listItem = findWithType(result, 'li');
+    //     expect(listItem).toBeDefined();
+    // }
+    // );
+
+
+
+    it('renders correctly with empty inputs', () => {
+        const result = renderApp();
+        expect(result).toMatchSnapshot();
+
+        expect(findAllWithType(result, Input).length).toBe(1);
+
+        // console.log("Button", findAllWithType(result, Input).length);
+
+        const button = findWithType(result, Button);
+        button.props.onPress(); 
+        // button.props.onPress();
+        // console.log("BUTTON PROB", button.props.onPress);
+
+        const output = renderer.getRenderOutput();
+        const task = findAllWithType(output, Input);
+        console.log( findAllWithType(output, Input).length);
+
+
+    });
+
 });
